743e9b1c250798402694a71d6ed69252a3c6756a
fabianlsz <fabianraul1904@gmail.com>
2025-04-15 23:56:40 +0300

    added time complexities for each function

--- Labs/Lab2_MultiMap20/MultiMap.cpp
+++ Labs/Lab2_MultiMap20/MultiMap.cpp
@@ -2,49 +2,146 @@
 #include "MultiMapIterator.h"
 #include <exception>
 #include <iostream>
+#include <vector> // Ensure vector is included for usage
 
 using namespace std;
 
-
+// Constructor
+// best = average = worst = teta 1
 MultiMap::MultiMap() {
-    //TODO - Implementation
+    this->head = nullptr;
+    this->tail = nullptr;
+    this->totalSize = 0;
 }
 
-
+// Adds a key-value pair to the multimap
+// best: teta 1 (key already exists and the value is added to an empty key value list)
+// avg: O(n+m) (traverseaza key list O(n) si adauga valoarea la fin. listei de valori O(m)
+// avg: O(n+m) (traverseaza key list O(n) si adauga valoarea la fin. listei de valori O(m)
 void MultiMap::add(TKey c, TValue v) {
-    //TODO - Implementation
+    KeyNode* keyNode = findKeyNode(c);
+    if (!keyNode) {
+        // Create a new KeyNode
+        KeyNode* newKeyNode = new KeyNode{c, nullptr, nullptr, nullptr};
+        if (!head) {
+            head = tail = newKeyNode;
+        } else {
+            tail->next = newKeyNode;
+            newKeyNode->prev = tail;
+            tail = newKeyNode;
+        }
+        // Create a ValueNode for the new key
+        newKeyNode->valueHead = new ValueNode{v, nullptr, nullptr};
+    } else {
+        // Append a ValueNode to the existing key's list
+        ValueNode* current = keyNode->valueHead;
+        while (current->next) current = current->next;
+        current->next = new ValueNode{v, nullptr, current};
+    }
+    totalSize++;
 }
 
-
+// Removes a key-value pair from the multimap
+//best: teta 1
+// worst = avg O(n+m_) la fel ca si la add motivul
 bool MultiMap::remove(TKey c, TValue v) {
-    //TODO - Implementation
-    return  false;
+    KeyNode* keyNode = findKeyNode(c);
+    if (!keyNode) return false;
+
+    ValueNode* valueNode = keyNode->valueHead;
+    while (valueNode && valueNode->value != v) valueNode = valueNode->next;
+
+    if (!valueNode) return false;
+
+    if (valueNode->prev) valueNode->prev->next = valueNode->next;
+    else keyNode->valueHead = valueNode->next;
+
+    if (valueNode->next) valueNode->next->prev = valueNode->prev;
+
+    delete valueNode;
+    totalSize--;
+
+    if (!keyNode->valueHead) {
+        if (keyNode->prev) keyNode->prev->next = keyNode->next;
+        else head = keyNode->next;
+
+        if (keyNode->next) keyNode->next->prev = keyNode->prev;
+        else tail = keyNode->prev;
+
+        delete keyNode;
+    }
+
+    return true;
 }
 
+// Returns the vector of values associated with a key
+// best: teta 1
+// avg = worst la fel motivul ca si mai sus
+std::vector<TValue> MultiMap::search(TKey c) const {
+    std::vector<TValue> result;
+    KeyNode* keyNode = findKeyNode(c);
+    if (!keyNode) return result;
 
-vector<TValue> MultiMap::search(TKey c) const {
-    //TODO - Implementation
-    return vector<TValue>();
+    ValueNode* valueNode = keyNode->valueHead;
+    while (valueNode) {
+        result.push_back(valueNode->value);
+        valueNode = valueNode->next;
+    }
+
+    return result;
 }
 
-
+// Returns the number of pairs in the multimap
+//best=avg=worst: teta 1
 int MultiMap::size() const {
-    //TODO - Implementation
-    return 0;
+    return totalSize;
 }
 
-
+// Checks whether the multimap is empty
+//best=avg=worst: teta 1
 bool MultiMap::isEmpty() const {
-    //TODO - Implementation
-    return false;
+    return totalSize == 0;
 }
 
+// Destructor
+// best: teta 1 daca multimap e empty
+//average = worst: O(n+m)
+MultiMap::~MultiMap() {
+    while (head) {
+        KeyNode* nextKeyNode = head->next;
+        deleteValueList(head->valueHead);
+        delete head;
+        head = nextKeyNode;
+    }
+    head = tail = nullptr;
+}
+
+// Helper to find a KeyNode by key
+// bst teta 1 daca key e prima din lista
+//average teta n - traverseaza aprox. jumatate din lista pentru gasirea key-ului
+//worst teta n daca key e ultima din lista
+KeyNode* MultiMap::findKeyNode(TKey key) const {
+    KeyNode* current = head;
+    while (current) {
+        if (current->key == key) return current;
+        current = current->next;
+    }
+    return nullptr;
+}
+
+// Helper to delete a ValueNode list
+// best: teta 1
+// worst = avg teta(m), traverseaza si sterge toate nodurile din lista, m=nr valori
+void MultiMap::deleteValueList(ValueNode* valueHead) {
+    while (valueHead) {
+        ValueNode* nextValueNode = valueHead->next;
+        delete valueHead;
+        valueHead = nextValueNode;
+    }
+}
+
+// Returns an iterator for the multimap
+// b = avg = w: teta 1
 MultiMapIterator MultiMap::iterator() const {
     return MultiMapIterator(*this);
 }
-
-
-MultiMap::~MultiMap() {
-    //TODO - Implementation
-}
-
--- Labs/Lab2_MultiMap20/MultiMapIterator.cpp
+++ Labs/Lab2_MultiMap20/MultiMapIterator.cpp
@@ -1,26 +1,53 @@
 #include "MultiMapIterator.h"
 #include "MultiMap.h"
+#include <iostream>
 
+using namespace std;
 
+// Constructor
+// best = average = worst = teta 1
 MultiMapIterator::MultiMapIterator(const MultiMap& c): col(c) {
-	//TODO - Implementation
+    // Set the iterator to point to the first key node in the MultiMap
+    currentKeyNode = col.head;
+
+    // Check if the MultiMap is empty. If not, set currentValueNode to the head of the value list for the first key.
+    if (currentKeyNode != nullptr) {
+        currentValueNode = currentKeyNode->valueHead;
+    } else {
+        // If the MultiMap is empty, there are no values to iterate over
+        currentValueNode = nullptr;
+    }
 }
 
-TElem MultiMapIterator::getCurrent() const{
-	//TODO - Implementation
-	return NULL_TELEM;
+// Returns the current element
+// best = average = worst = teta 1
+TElem MultiMapIterator::getCurrent() const {
+    if (!valid()) throw std::exception();
+    return {currentKeyNode->key, currentValueNode->value};
 }
 
+// Checks if the iterator is valid
+// best = average = worst = teta 1
 bool MultiMapIterator::valid() const {
-	//TODO - Implementation
-	return false;
+    return currentKeyNode != nullptr && currentValueNode != nullptr;
 }
 
+// Moves to the next element
+// best = average = worst = teta 1
 void MultiMapIterator::next() {
-	//TODO - Implementation
+    if (!valid()) throw std::exception();
+
+    if (currentValueNode->next) {
+        currentValueNode = currentValueNode->next;
+    } else {
+        currentKeyNode = currentKeyNode->next;
+        currentValueNode = currentKeyNode ? currentKeyNode->valueHead : nullptr;
+    }
 }
 
+// Moves to the first element
+// best = average = worst = teta 1
 void MultiMapIterator::first() {
-	//TODO - Implementation
+    currentKeyNode = col.head;
+    currentValueNode = currentKeyNode ? currentKeyNode->valueHead : nullptr;
 }
-
